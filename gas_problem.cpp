f997b78fd17e6426b735fad3bd1e6537cb2fd0d9
greatbridf <guobingfang20040616@gmail.com>
2019-01-19 14:48:37 +0800

    Create gas_problem.cpp

---
+++ gas_problem.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int n, k;
+  cin >> n >> k;
+  int* station_distance = new int[k+1];
+  for (int i = 0; i < k+1; ++i) {
+    cin >> station_distance[i];
+    if (station_distance[i] > n) {
+      cout << "No Solution!";
+      return 0;
+    }
+  }
+  int result = 0, gas_life = n;
+  for (int i = 0; i < k+1; ++i) {
+    if (gas_life - station_distance[i] < 0) {
+      ++result;
+      gas_life = n - station_distance[i];
+    } else
+      gas_life -= station_distance[i];
+  }
+  cout << result;
+  delete [] station_distance;
+  return 0;
+}

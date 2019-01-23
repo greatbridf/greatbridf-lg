051cc9dc4171a7dd7850b44c63b7b9f18f44aa74
greatbridf <guobingfang20040616@gmail.com>
2019-01-23 22:40:11 +0800

    Update prime_loop.cpp

--- prime_loop.cpp
+++ prime_loop.cpp
@@ -2,36 +2,47 @@
 #include <cstdio>
 #include <algorithm>
 
+#define __macos
+
+#ifdef __macos
+#define __gcd __algo_gcd
+#endif
+
 using namespace std;
 
-bool visited[16] = {false};
-int result[16];
+bool visited[20] = {true, false};
+int result[20] = {1};
+int END;
 
-bool is_prime(int a, int b) {
-  return __gcd(a, b) == 1;
+bool is_prime(int a) {
+  if (a <= 3) return true;
+  for (int i = 2; i < a; ++i)
+    if (a%i==0)
+      return false;
+  return true;
 }
 
-void prime(int END, int n) {
-  if (n == END + 1) {
+void prime(int n) {
+  if (n == END && is_prime(result[0]+result[n-1])) {
     for (int i = 0; i < END; ++i)
       printf("%d ", result[i]);
     cout << endl;
     return;
   }
-  for (int i = 0; i < END; ++i) {
-    if (!visited[i] && (i == END-1 || is_prime(i, result[i+1]))) {
-      visited[i] = true;
-      result[i] = i+1;
-      prime(END, n+1);
-      visited[i] = false;
+  for (int i = 2; i <= END; ++i) {
+    if (!visited[i-1] && is_prime(result[n-1]+i)) {
+      visited[i-1] = true;
+      result[n] = i;
+      prime(n+1);
+      visited[i-1] = false;
     }
   }
 }
 
 int main() {
-  int END;
-  cin >> END;
-  prime(END, 1);
+  while (cin >> END) {
+    prime(1);
+  }
   return 0;
 }
 

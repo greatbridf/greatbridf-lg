301b16cb25b84f0f49f14e94b05216c6c2433090
greatbridf <guobingfang20040616@gmail.com>
2019-01-24 22:49:24 +0800

    Add 22.cpp

---
+++ cn.edu.hrbeu.noip/22.cpp
@@ -0,0 +1,27 @@
+// Unfinished
+#include <iostream>
+
+using namespace std;
+
+bool holes[1000];
+
+int main() {
+  int nHole, nTimes, t = 0;
+  cin >> nHole >> nTimes;
+  for (int i = 0; i < nTimes; ++i) {
+    if (t%nHole == 0) holes[nHole] = true;
+    else holes[t%nHole] = true;
+    t += (i+1);
+  }
+  int flag = 0;
+  for (int i = 1; i <= nHole; ++i) {
+    if (!holes[i]) {
+      ++flag;
+      cout << i << " ";
+    }
+  }
+  if (flag == 0)
+    cout << 0;
+  cout << endl;
+  return 0;
+}

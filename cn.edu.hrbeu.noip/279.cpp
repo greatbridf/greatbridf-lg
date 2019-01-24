07ad5d7aeaa3c42ecc3e04602099c00bbc7f2ca9
greatbridf <guobingfang20040616@gmail.com>
2019-01-24 13:38:12 +0800

    Update 279.cpp

--- cn.edu.hrbeu.noip/279.cpp
+++ cn.edu.hrbeu.noip/279.cpp
@@ -1,40 +1,34 @@
+// Figure out what's fuckin' going on there
 #include <iostream>
 
 using namespace std;
 
 int result[11];
 
-int m, limit;
+int m, sum = 0;
 
-int sum = 0, result_count = 0;
-
-int debug = 0;
-
-void dfs(int fir) {
+void dfs(int n) {
   if (sum == m) {
     cout << result[0];
-    for (int i = 1; i < result_count; ++i) {
+    for (int i = 1; i < n-1; ++i) {
       cout << "+" << result[i];
     }
     cout << endl;
     return;
   }
   for (int i = 1; i < m; ++i) {
-    if (sum < m && (result_count == 0 || result[result_count-1] <= i)) {
-      result[result_count] = i;
+    if (sum < m && (n == 1 || result[n-2] <= i)) {
+      result[n-1] = i;
       sum += i;
-      ++result_count;
-      dfs(fir+1);
-      --result_count;
+      dfs(n+1);
       sum -= i;
-      result[result_count] = 0;
+      result[n-1] = 0;
     }
   }
 }
 
 int main() {
   while (cin >> m) {
-    limit = m / 2;
     dfs(1);
   }
   return 0;

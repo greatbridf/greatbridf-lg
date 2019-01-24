5cfda252c851becec5bbb11c7b2ce2178c98845c
greatbridf <guobingfang20040616@gmail.com>
2019-01-24 23:13:40 +0800

    Update 跳棋的挑战.cpp

--- DFS/跳棋的挑战.cpp
+++ DFS/跳棋的挑战.cpp
@@ -8,20 +8,39 @@ bool neg[25];
 
 int nBSize = 0;
 
+int ans[13];
+int nCount = 0;
+
 void dfs(int line) {
+    if (line == nBSize) {
+        if (nCount < 3) {
+           for (int i = 0; i < nBSize; ++i) {
+              cout << ans[i]+1 << " ";
+           } 
+           cout << endl;
+        }
+        ++nCount;
+        return;
+    }
     for (int nCol = 0; nCol < nBSize; ++nCol) {
         if (!col[nCol] && !pos[line+nCol] && !neg[line-nCol+nBSize-1]) {
             col[nCol] = true;
             pos[line+nCol] = true;
             neg[line-nCol+nBSize-1] = true;
+            ans[line] = nCol;
             dfs(line+1);
+            col[nCol] = false;
+            pos[line+nCol] = false;
+            neg[line-nCol+nBSize-1] = false;
         }
     }
 }
 
 int main() {
     while (cin >> nBSize) {
+        nCount = 0;
         dfs(0);
+        cout << nCount << endl;
     }
     return 0;
 }
